3e7c46e4ed56edd4d0f8efef55b9dffad325b157
shakurbol <bolakokbol@gmail.com>
2023-09-26 16:23:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningGame = record.find(game => game.result === "W");
+  
+    if (winningGame) {
+      return winningGame.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  
+  const superBowlRecords = [
+    { year: 2020, result: "L" },
+    { year: 2021, result: "W" },
+    { year: 2022, result: "L" },
+  ];
+  
+  const winningYear = superbowlWin(superBowlRecords);
+  console.log(winningYear); 
+  
